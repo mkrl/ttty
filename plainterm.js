7112b1b0de9708406b83d90f1234e2f2c5092951
Mikhail <tradecontent@gmail.com>
2018-10-14 04:44:53 +0800

    Basic command typing

--- plainterm.js
+++ plainterm.js
@@ -1,5 +1,5 @@
 //Declare master object
-var bash = {commands: {}, 
+var bash = {commands: {},
     version: "0.1"
 }
 
@@ -43,21 +43,38 @@ function term_init(settings) {
     settings.id = settings.id || "terminal";
     settings.welcome = settings.welcome || "plainterm.js v. " + bash.version;
     settings.prompt = settings.prompt || "$ ";
-    println(settings.welcome);
     for (o of Object.keys(settings.commands)) {
         var cmd = settings.commands[o];
         bash.commands[cmd.name] = new Command(cmd.name, cmd.description, cmd.parameters, cmd.func);
     }
     var container = document.createElement("div");
-    var input_container = document.createElement("input");
+    var input_container = document.createElement("div");
+    var prompt_span = document.createElement("span");
+    prompt_span.innerHTML = settings.prompt;
+    var input = document.createElement("input");
     container.className = "terminal-container";
     input_container.className = "terminal-type";
-    input_container.setAttribute("type", "text");
+    input.setAttribute("type", "text");
+    input.setAttribute("onkeypress", "run(this)");
     document.getElementById(settings.id).appendChild(container);
     document.getElementById(settings.id).appendChild(input_container);
+    for (e of document.getElementsByClassName("terminal-type")) {
+        e.appendChild(prompt_span);
+        e.appendChild(input);
+    }
+    bash.container = document.getElementById(settings.id).firstElementChild;
 }
 
 //Prints a newline in console
 function println(content) {
-    console.log(content)
+    var cmd = document.createElement("p");
+    cmd.innerHTML = settings.prompt + content;
+    bash.container.appendChild(cmd);
+}
+
+//Reads text on Enter press
+function run(cmd) {
+    if(event.keyCode == 13) {
+        println(cmd.value)
+    }
 }
