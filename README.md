4d3ff9e8ecbef29f6ac4478a40eda473b7860d39
Mikhail <tradecontent@gmail.com>
2018-10-15 00:56:18 +0800

    Rewrite as flexible object
    - Proper API
    - Basic readable docs
    - Fixed history not counting args
    - Improved things
    - More generic demo page

--- README.md
+++ README.md
@@ -23,42 +23,36 @@ var settings = {
             name: "echo", //Command name
             description: "a test command with one echo arg", //Help text to be displayed when `help` command is called
             parameters: ["a string to be echoed in console"], //An optional array of successive parameter descriptions, used when the command that needs args is being called without any args
-            func: function(params){println(params[0])} //Function to be called when the command is executed. Accepts an array of parameters, ordered in the same way as in the previous property
+            func: function(params){plainterm.print(params[0])} //Function to be called when the command is executed. Accepts an array of parameters, ordered in the same way as in the previous property
         },
         test: {
             name: "test", 
             description: "a test command with no args", 
-            func: function(){println("testing things")} 
+            func: function(){plainterm.print("testing things")} 
         },
         multiply: {
             name: "multiply",
             description: "Multiply two numbers",
             parameters: ["number one", "number two"],
-            func: function(params){println(params[0]*params[1])}
+            func: function(params){plainterm.print(params[0]*params[1])}
         }
     }
 };
 
-term_init(settings);
+plainterm.init(settings);
 ```
 
 ## Working with the terminal:
 
 `help` - Display a list of all commands with descriptions
+
 `command` - Execute a command. Will display "Usage: command [parameter 1 description] [parameter 2 description], etc.", when needs args but called without them.
 
 ## API:
 
-`println(text, command)` - Print a text in terminal starting from a new line. Command - optional, count given string as a command (prepend prompt, syntax highlight).
-
-`bash.version` - plainterm.js version
-`bash.commands` - an object containing all available commands available
-
-
-`bash.commands.[command].name` - command name
-
-`bash.commands.[command].description` - command description
-
-`bash.commands.[command].parameters` - command parameters
-
-`bash.commands.[command].func` - retrieve a function. call with `bash.commands.[command].func()`
+| Method  | Description | Parameters |
+| ------------- | ------------- | ------------- |
+| `init(settings)`  | Initialize a terminal in a DOM with given ID | Object. See the example above |
+| `print(text, c)`  | Prints a given text in the terminal  | `text` - String, `c` - Boolean, optional, defaults to false. Count given string as a command (displays prompt, syntax highlight and appears in history) |
+| `run(text)`  | Emulates a command execution in a terminal (acts the same way as a use would have typed and pressed Enter)  | `text` - String |
+| `hist(up)`  | Search in command history. Returns string.  | `up` - Boolean, optional. Defaults to true. Upward/downward search. |
--- plainterm.js
+++ plainterm.js
@@ -1,145 +1,173 @@
 /* plainterm.js
 https://github.com/mkrl/plainterm.js */
 
-//Declare master object
-var bash = {commands: {}, history: [], last: 0,
-    version: "0.3"
-}
 
-//Master command constructor
-function Command(name, description, parameters = [], func) {
-    this.name = name;
-    this.description = description;
-    this.parameters = parameters;
-    if (parameters.length > 0) {
-        var pnames = "";
-        for (p of parameters) {
-            pnames = pnames + " [" + p + "]";
-        }
-        this.func = function(params) {
-            params = params || false;
-            if (params == false) {
-            println("usage: " + name + pnames);
-            } else {
-                func(params);
-            }
-        }
-    } else {
-        this.func = func;
+var plainterm = (function() {
+
+    //Master object
+    var bash = {commands: {}, history: [], last: 0,
+        version: "0.4"
     }
-}
 
-//Constructing help command
-bash.commands.help = new Command("help","shows a help message", [],
-    function(){
-        Object.getOwnPropertyNames(bash.commands).map(function(cmd){
-           println(bash.commands[cmd].name + " - " + bash.commands[cmd].description);
-        });
-    });
+    //Master command constructor
+    function Command(name, description, parameters = [], func) {
+        this.name = name;
+        this.description = description;
+        this.parameters = parameters;
+        if (parameters.length > 0) {
+            var pnames = "";
+            for (p of parameters) {
+                pnames = pnames + " [" + p + "]";
+            }
+            this.func = function(params) {
+                params = params || false;
+                if ((params == false)||(params.length < parameters.length)) {
+                println("usage: " + name + pnames);
+                } else {
+                    func(params);
+                }
+            }
+        } else {
+            this.func = func;
+        }
+    }
+
+    //Utility functions
+
+    //Initialize terminal from parameters
+    function term_init(settings) {
+        settings.id = settings.id || "terminal";
+        settings.welcome = settings.welcome || "plainterm.js v. " + bash.version;
+        settings.prompt = settings.prompt || "$ ";
+        for (o of Object.keys(settings.commands)) {
+            var cmd = settings.commands[o];
+            addcommand(cmd);
+        }
+        build_tree(document.getElementById(settings.id));
+        println(settings.welcome);
+    }
 
 
-//Utility functions
+    //Build terminal DOM tree
+    function build_tree(dom) {
+        dom.className = "terminal";
+        var container = document.createElement("div");
+        var input_container = document.createElement("div");
+        var prompt_span = document.createElement("span");
+        prompt_span.innerHTML = settings.prompt;
+        var input = document.createElement("input");
+        container.className = "terminal-container";
+        input_container.className = "terminal-type";
+        input.setAttribute("type", "text");
+        input.setAttribute("onkeypress", "plainterm.run(this)");
+        dom.appendChild(container);
+        dom.appendChild(input_container);
+        for (e of document.getElementsByClassName("terminal-type")) {
+            e.appendChild(prompt_span);
+            e.appendChild(input);
+        }
+        bash.container = dom.firstElementChild;
+        bash.container.input = input;
+        dom.onclick = function() {input.focus()}
+    }
 
-//Initialize terminal from parameters
-function term_init(settings) {
-    settings.id = settings.id || "terminal";
-    settings.welcome = settings.welcome || "plainterm.js v. " + bash.version;
-    settings.prompt = settings.prompt || "$ ";
-    for (o of Object.keys(settings.commands)) {
-        var cmd = settings.commands[o];
+    function addcommand(cmd) {
         bash.commands[cmd.name] = new Command(cmd.name, cmd.description, cmd.parameters, cmd.func);
     }
-    build_tree(document.getElementById(settings.id));
     
-}
-
-
-//Build terminal DOM tree
-function build_tree(dom) {
-    var container = document.createElement("div");
-    var input_container = document.createElement("div");
-    var prompt_span = document.createElement("span");
-    prompt_span.innerHTML = settings.prompt;
-    var input = document.createElement("input");
-    container.className = "terminal-container";
-    input_container.className = "terminal-type";
-    input.setAttribute("type", "text");
-    input.setAttribute("onkeypress", "run(this)");
-    dom.appendChild(container);
-    dom.appendChild(input_container);
-    for (e of document.getElementsByClassName("terminal-type")) {
-        e.appendChild(prompt_span);
-        e.appendChild(input);
-    }
-    bash.container = dom.firstElementChild;
-    bash.container.input = input;
-    dom.onclick = function() {input.focus()}
-}
-
-//Prints a newline in console
-function println(content, c = false) {
-    var line = document.createElement("p");
-    if (c === true) {
-        var cmd = document.createElement("span");
-        cmd.innerHTML = content;
-        cmd.className = "command";
-        line.innerHTML = settings.prompt;
-        bash.container.appendChild(line);
-        line.appendChild(cmd);
-    } else {
-        line.innerHTML = content;
-        bash.container.appendChild(line);
-    }
-}
-
-//Eval as command
-function evaluate(cmd) {
-    comms = cmd.split(/\s+/);
-    comm = comms[0];
-    comms.shift();
-    if (Object.getOwnPropertyNames(bash.commands).indexOf(comm) > -1) {
-        bash.commands[comm].func(comms);
-        bash.history.unshift(comm);
-    } else {
-        println(comm + " - command not found")
-    }
-}
-
-//Reads text on Enter press
-function run(cmd) {
-    if(event.keyCode == 13) {
-        println(cmd.value, true);
-        if (cmd.value.length > 0) {
-            evaluate(cmd.value)
-        }
-        cmd.value = "";
-        bash.last = 0;
-        bash.container.input.scrollIntoView();
-    }
-}
-
-//History
-
-function get_from_history(up = true) {
-    if (bash.history.length > 0) {
-        if (up === true && bash.last < bash.history.length-1) {
-            bash.container.input.value = bash.history[bash.last];
-            bash.last++;
-        } else if (up === false && bash.last > 0){
-            bash.container.input.value = bash.history[bash.last];
-            bash.last--;
+    
+    //Prints a newline in console
+    function println(content, c = false) {
+        var line = document.createElement("p");
+        if (c === true) {
+            var cmd = document.createElement("span");
+            cmd.innerHTML = content;
+            cmd.className = "command";
+            line.innerHTML = settings.prompt;
+            bash.container.appendChild(line);
+            line.appendChild(cmd);
+        } else {
+            line.innerHTML = content;
+            bash.container.appendChild(line);
         }
     }
-}
 
-document.addEventListener('keydown', function(event) {
-    const key = event.key;
-    switch (event.key) {
-        case "ArrowUp":
-            get_from_history();
-            break;
-        case "ArrowDown":
-            get_from_history(false);
-            break;
+    //Eval as command
+    function evaluate(cmd) {
+        comms = cmd.split(/\s+/);
+        comm = comms[0];
+        comms.shift();
+        if (Object.getOwnPropertyNames(bash.commands).indexOf(comm) > -1) {
+            bash.commands[comm].func(comms);
+            bash.history.unshift(cmd);
+        } else {
+            println(comm + " - command not found")
+        }
     }
-});
+
+    //Reads text on Enter press
+    function run(cmd) {
+        if(event.keyCode == 13) {
+            println(cmd.value, true);
+            if (cmd.value.length > 0) {
+                evaluate(cmd.value)
+            }
+            cmd.value = "";
+            bash.last = 0;
+            bash.container.input.scrollIntoView();
+        }
+    }
+
+    //History
+
+    function get_from_history(up = true) {
+        if (bash.history.length > 0) {
+            if (up === true && bash.last < bash.history.length-1) {
+                bash.container.input.value = bash.history[bash.last];
+                bash.last++;
+            } else if (up === false && bash.last > 0){
+                bash.container.input.value = bash.history[bash.last];
+                bash.last--;
+            }
+        }
+    }
+
+    document.addEventListener('keydown', function(event) {
+        const key = event.key;
+        switch (event.key) {
+            case "ArrowUp":
+                get_from_history();
+                break;
+            case "ArrowDown":
+                get_from_history(false);
+                break;
+        }
+    });
+
+    //Constructing default help command
+    bash.commands.help = new Command("help","shows a help message", [],
+        function(){
+            Object.getOwnPropertyNames(bash.commands).map(function(cmd){
+               println(bash.commands[cmd].name + " - " + bash.commands[cmd].description);
+            });
+        });
+
+    //API
+    return {
+        init: term_init,
+        run: run,
+        print: println,
+        hist: get_from_history
+    }
+
+})();
+
+
+
+
+
+
+
+
+
+
