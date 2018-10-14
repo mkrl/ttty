21cf82905e2eb5e083cfd9ab06b5346a54807b46
Mikhail <tradecontent@gmail.com>
2018-10-14 15:06:30 +0800

    Optional parameters, autofocus

--- README.md
+++ README.md
@@ -19,17 +19,16 @@ var settings = {
     welcome: "Welcome to plainterm.js terminal emulator", //A welcome message that is being printed on initialization
     prompt: "user@mkrl.xyz:~$",
     commands: {
-        test: {
-            name: "test", //Command name
-            description: "A test command with no args", //Help text to be displayed when `help` command is called
-            parameters: [], //An array of successive parameter descriptions, used when the command that needs args is being called without any args
-            func: function(){println("testing things")} //Function to be called when the command is executed. Accepts an array of parameters, ordered in the same way as in the previous property (see the next command for example)
-        },
         echo: {
-            name: "echo",
-            description: "A test command with one echo arg",
-            parameters: ["a string to be echoed in console"],
-            func: function(params){println(params[0])}
+            name: "echo", //Command name
+            description: "A test command with one echo arg", //Help text to be displayed when `help` command is called
+            parameters: ["a string to be echoed in console"], //An optional array of successive parameter descriptions, used when the command that needs args is being called without any args
+            func: function(params){println(params[0])} //Function to be called when the command is executed. Accepts an array of parameters, ordered in the same way as in the previous property
+        },
+        test: {
+            name: "test", 
+            description: "A test command with no args", 
+            func: function(){println("testing things")} 
         }
     }
 };
@@ -47,9 +46,11 @@ term_init(settings);
 `println(text)` - Print a text in terminal starting from a new line
 
 `bash.version` - plainterm.js version
-`bash.commands` - an object containing all the available commands
+`bash.commands` - an object containing all available commands available
+
 `bash.commands.command`: 
-    `bash.commands.command.name` - command name
-    `bash.commands.command.description` - command description
-    `bash.commands.command.parameters` - command parameters
-    `bash.commands.command.func` - retrieve a function. call with `bash.commands.command.func()`
+
+`bash.commands.command.name` - command name
+`bash.commands.command.description` - command description
+`bash.commands.command.parameters` - command parameters
+`bash.commands.command.func` - retrieve a function. call with `bash.commands.command.func()`
--- plainterm.js
+++ plainterm.js
@@ -1,10 +1,13 @@
+/* plainterm.js
+https://github.com/mkrl/plainterm.js */
+
 //Declare master object
-var bash = {commands: {},
+var bash = {commands: {}, 
     version: "0.2"
 }
 
 //Master command constructor
-function Command(name, description, parameters, func) {
+function Command(name, description, parameters = [], func) {
     this.name = name;
     this.description = description;
     this.parameters = parameters;
@@ -35,7 +38,6 @@ bash.commands.help = new Command("help","Shows a help message", [],
     });
 
 
-
 //Utility functions
 
 //Initialize terminal from parameters
@@ -47,6 +49,13 @@ function term_init(settings) {
         var cmd = settings.commands[o];
         bash.commands[cmd.name] = new Command(cmd.name, cmd.description, cmd.parameters, cmd.func);
     }
+    build_tree(document.getElementById(settings.id));
+    
+}
+
+
+//Build terminal DOM tree
+function build_tree(dom) {
     var container = document.createElement("div");
     var input_container = document.createElement("div");
     var prompt_span = document.createElement("span");
@@ -56,13 +65,15 @@ function term_init(settings) {
     input_container.className = "terminal-type";
     input.setAttribute("type", "text");
     input.setAttribute("onkeypress", "run(this)");
-    document.getElementById(settings.id).appendChild(container);
-    document.getElementById(settings.id).appendChild(input_container);
+    dom.appendChild(container);
+    dom.appendChild(input_container);
     for (e of document.getElementsByClassName("terminal-type")) {
         e.appendChild(prompt_span);
         e.appendChild(input);
     }
-    bash.container = document.getElementById(settings.id).firstElementChild;
+    bash.container = dom.firstElementChild;
+    bash.container.input = input_container;
+    dom.onclick = function() {input.focus()}
 }
 
 //Prints a newline in console
@@ -70,15 +81,6 @@ function println(content) {
     var cmd = document.createElement("p");
     cmd.innerHTML = content;
     bash.container.appendChild(cmd);
-}
-
-//Reads text on Enter press
-function run(cmd) {
-    if(event.keyCode == 13) {
-        println(settings.prompt + cmd.value);
-        evaluate(cmd.value);
-        cmd.value = "";
-    }
 }
 
 //Eval as command
@@ -92,3 +94,16 @@ function evaluate(cmd) {
         println(comm + " - command not found")
     }
 }
+
+//Reads text on Enter press
+function run(cmd) {
+    if(event.keyCode == 13) {
+        println(settings.prompt + cmd.value);
+        if (cmd.value.length > 0) {
+            evaluate(cmd.value)
+        }
+        cmd.value = "";
+        bash.container.input.scrollIntoView();
+    }
+}
+
