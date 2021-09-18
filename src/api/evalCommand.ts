9b5287ee5d13179136618795aca98dbe39023109
Mikhail Korolev <mikhail.korolev@toptal.com>
2021-09-18 18:36:46 +0200

    Implement command arguments, improve command output

--- src/api/evalCommand.ts
+++ src/api/evalCommand.ts
@@ -2,11 +2,24 @@ import { Terminal } from '../types'
 import { dispatchEvent, TerminalEvent } from '../helpers/events'
 
 const evalCommand = (cmd: string, instance: Terminal) => {
+  instance.print(cmd, true)
   const splitCommand = cmd.split(' ')
   const command = splitCommand[0]
-  if (instance.settings.commands[command]) {
-    instance.settings.commands[command].func(instance)
-    dispatchEvent(TerminalEvent.ON_COMMAND, instance.settings.host)
+  const commandArguments = splitCommand.slice(1)
+
+  const commandInstance = instance.settings.commands[command]
+
+  if (commandInstance) {
+    if (commandInstance.argDescriptions &&
+      commandInstance.argDescriptions?.length > 0 &&
+      commandArguments.length === 0
+    ) {
+      instance.print(`Usage: ${command} ${commandInstance
+        .argDescriptions?.map(arg => `[${arg}]`).join(' ')}`)
+    } else {
+      commandInstance.func(instance, ...commandArguments)
+      dispatchEvent(TerminalEvent.ON_COMMAND, instance.settings.host)
+    }
   } else {
     instance.print(`<span class="terminal-error">command not found: ${command}</span>`)
     dispatchEvent(TerminalEvent.ON_COMMAND_NOT_FOUND, instance.settings.host)
