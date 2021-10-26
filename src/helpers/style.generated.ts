db0188f1704a9fea85e9b0304f4f496ce8fe14e5
Mikhail Korolev <mikhail.korolev@toptal.com>
2021-10-26 15:29:31 +0200

    fix(styling): add css variables with fallback

--- src/helpers/style.generated.ts
+++ src/helpers/style.generated.ts
@@ -3,6 +3,6 @@
   Do not modify it directly. To make style changes please go to ./src/ttty.css
 */
 const TERMINAL_STYLE =
-  '.terminal{overflow-y:auto;overflow-x:hidden;background-color:#000;color:#fff;padding:15px;box-sizing:border-box;font-family:"Courier New",monospace}.terminal-type{width:100%;display:flex}.terminal-type>span{padding-right:7pt}.terminal-type>input{flex-grow:2;background:transparent;border:0;color:#fff;font-family:"Courier New",monospace;font-size:16px}.terminal-type>input:focus{border:none;outline:none}.terminal-container>p{margin:0}span.terminal-prompt{color:#929292}span.terminal-command{color:#ffff7d}span.terminal-error{color:#cc1010}'
+  '.terminal{overflow-y:auto;overflow-x:hidden;background-color:var(--terminal-bg-color, black);padding:15px;box-sizing:border-box}.terminal *{color:var(--terminal-fg-color, white);font-family:var(--terminal-font, "Courier New", monospace)}.terminal-type{width:100%;display:flex}.terminal-type>span{padding-right:7pt}.terminal-type>input{flex-grow:2;background:transparent;border:0;font-size:inherit}.terminal-type>input:focus{border:none;outline:none}.terminal-container>p{margin:0}span.terminal-command{color:var(--terminal-accent-color, #ffff7d)}span.terminal-error{color:var(--terminal-error-color, #cc1010)}'
 
 export default TERMINAL_STYLE
