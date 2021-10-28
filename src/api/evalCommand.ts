9d0c272eb8952a7c89398000636092ce3c446a8d
iamrichr <44210678+iamrichR@users.noreply.github.com>
2021-10-27 20:12:13 -0400

    Further refactoring of input parsing

--- src/api/evalCommand.ts
+++ src/api/evalCommand.ts
@@ -3,13 +3,12 @@ import { dispatchEvent, TerminalEvent } from '../helpers/events'
 
 const evalCommand = (cmd: string, instance: TerminalInstance) => {
   const { print } = instance
-  const splitCommand = cmd.split(' ')
-  const command = splitCommand[0]
-  const commandArgumentsString = splitCommand.slice(1).join(' ')
-  const argRegex = /('[^']+'|"[^"]+"|[^\s'"]+)/g
-  const argMatches = commandArgumentsString.match(argRegex)
-  const commandArguments = (argMatches === null ? [] : argMatches)
-    .map(element => element.replace(/(^['"]|['"]$)/g, ''))
+  const [command, ...args] = cmd.split(' ')
+  const argMatches = args.join(' ').match(/('[^']+'|"[^"]+"|[^\s'"]+)/g)
+  const commandArguments = argMatches === null
+    ? []
+    : argMatches
+      .map(element => element.replace(/(^['"]|['"]$)/g, ''))
   const commandInstance = instance.settings.commands[command]
 
   if (commandInstance) {
